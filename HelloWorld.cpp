41eb87b773a27e2d0a9da9d7efc67d977e92a4e6
CrazyYong <504500534@qq.com>
2017-12-21 15:18:18 +0800

    第一次测试提交

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>    // 相当于 java的import .h c的头文件 stdio.h standard io 标准输入输出 
+#include<stdlib.h>   // stdlib standard library 标准函数库    java.lang 
+/**
+*/
+main(){    // public static void main(String[] args)
+       printf("helloworld!\n");  //System.out.println();   "\n"换行符 
+       system("javac Hello.java");
+       system("java Hello");
+       system("notepad");
+       system("pause"); //system执行windows的系统命令 
+       } 
+
